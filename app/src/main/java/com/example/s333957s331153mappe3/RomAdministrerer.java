438b5aee551a743ba28b2217d4c625abd398e7db
nikolasekiw <nsekiw@gmail.com>
2020-11-04 11:40:21 +0100

    json

--- app/src/main/java/com/example/s333957s331153mappe3/RomAdministrerer.java
+++ app/src/main/java/com/example/s333957s331153mappe3/RomAdministrerer.java
@@ -2,7 +2,6 @@ package com.example.s333957s331153mappe3;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Spinner;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RomAdministrerer extends AppCompatActivity {
@@ -18,5 +17,9 @@ public class RomAdministrerer extends AppCompatActivity {
         romNr = (EditText) findViewById(R.id.romNr);
         kapasitet = (EditText) findViewById(R.id.kapasitet);
         beskrivelse = (EditText) findViewById(R.id.beskrivelseRom);
+
+        getJSON task = new getJSON();
+        task.execute(new String[]{"http://student.cs.hioa.no/~s333975/Romjsonout.php"});
+
     }
 }
