cb11d01bd5da44405e3593e3bce08d5f0776fded
synneangell <44602964+synneangell@users.noreply.github.com>
2020-11-11 13:37:27 +0100

    Prøvd å hente adresse fra koordinater

--- app/src/main/java/com/example/s333957s331153mappe3/HusAdministrerer.java
+++ app/src/main/java/com/example/s333957s331153mappe3/HusAdministrerer.java
@@ -1,5 +1,7 @@
 package com.example.s333957s331153mappe3;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,15 +21,21 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+import java.util.Locale;
 
 public class HusAdministrerer extends AppCompatActivity {
     Spinner etasjer;
-    TextView koordinater;
-    EditText navn, beskrivelse, gateadresse;
+    TextView koordinater, gateadresse;
+    EditText navn, beskrivelse;
     LatLng innKoordinater;
+    Geocoder geocoder;
+    List<Address> adresser;
+    String adresse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +45,7 @@ public class HusAdministrerer extends AppCompatActivity {
         koordinater = (TextView) findViewById(R.id.koordinater);
         navn = (EditText) findViewById(R.id.navnBygning);
         beskrivelse = (EditText) findViewById(R.id.beskrivelseBygning);
-        gateadresse = (EditText) findViewById(R.id.adresseBygning);
+        gateadresse = (TextView) findViewById(R.id.adresseBygning);
         etasjer = (Spinner) findViewById(R.id.spinnerEtasjer);
 
         Integer[] items = new Integer[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
@@ -45,7 +53,18 @@ public class HusAdministrerer extends AppCompatActivity {
         etasjer.setAdapter(adapter);
 
         innKoordinater = getIntent().getExtras().getParcelable("koordinater");
-        koordinater.setText(innKoordinater.toString());
+        koordinater.setText("Koordinater: "+innKoordinater.latitude+", "+innKoordinater.longitude);
+        geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            adresser = geocoder.getFromLocation(innKoordinater.latitude, innKoordinater.latitude, 1);
+        } catch (IOException e) {
+            Log.d("TAG", "Fant ikke adresse til koordinater");
+            e.printStackTrace();
+        }
+        adresse = adresser.get(0).getAddressLine(0);
+        Log.d("Adresse",adresse);
+        gateadresse.setText(adresse);
+
 
     }
 
