67327e646448c465eb61acb152e739d5fdcd9dda
synneangell <44602964+synneangell@users.noreply.github.com>
2020-11-12 13:21:30 +0100

    Fjern denne comitten

--- app/src/main/java/com/example/s333957s331153mappe3/RomJSON.java
+++ app/src/main/java/com/example/s333957s331153mappe3/RomJSON.java
@@ -1,4 +1,79 @@
 package com.example.s333957s331153mappe3;
 
-public class RomJSON {
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RomJSON extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        RomJSON.getJSON task = new RomJSON.getJSON();
+        task.execute(new String[]{"http://student.cs.hioa.no/~s331153/romjsonout.php"});
+    }
+
+    private class getJSON extends AsyncTask<String, Void,String> {
+        List<Rom> alleRom = new ArrayList<>();
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String retur = "";
+            String s = "";
+            String output = "";
+            for (String url : urls) {
+
+                try {
+                    URL urlen = new URL(urls[0]);
+                    HttpURLConnection conn = (HttpURLConnection)
+                            urlen.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Accept", "application/json");
+
+                    if (conn.getResponseCode() != 200) {
+                        throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+                    }
+                    BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+                    System.out.println("Output from Server .... \n");
+                    while ((s = br.readLine()) != null) {
+                        output = output + s;
+                    }
+                    conn.disconnect();
+
+                    try {
+                        JSONArray mat = new JSONArray(output);
+                        for (int i = 0; i < mat.length(); i++) {
+                            JSONObject jsonobject = mat.getJSONObject(i);
+                            int romID = jsonobject.getInt("RomID");
+                            int husID = jsonobject.getInt("HusID");
+                            int etasjeNr = jsonobject.getInt("EtasjeNr");
+                            int romNr = jsonobject.getInt("RomNr");
+                            int kapasitet = jsonobject.getInt("Kapasitet");
+                            String beskrivelse = jsonobject.getString("Beskrivelse");
+                            Rom etRom = new Rom(romID, husID, etasjeNr, romNr, kapasitet, beskrivelse);
+                            alleRom.add(etRom);
+                        }
+                        return retur;
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    return retur;
+                } catch (Exception e) {
+                    return "Noe gikk feil";
+                }
+            }
+            return retur;
+        }
+    }
 }
